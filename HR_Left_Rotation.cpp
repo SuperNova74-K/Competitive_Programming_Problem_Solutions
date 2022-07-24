a5dfde54ca544f6cd7f90f00a58225ac167d62a0
SuperNova74-K <kingkhaled.w@gmail.com>
2022-07-24 15:17:53 +0200

    added another solution

--- HR_Left_Rotation.cpp
+++ HR_Left_Rotation.cpp
@@ -6,23 +6,49 @@
 
 using namespace std;
 
+//int main() {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(0);
+//    cout.tie(0);
+//
+//
+//    int a,b;
+//    cin >> a >> b;
+//    int arr[100005];
+//    for (int i = 0; i < a; ++i) {
+//        cin >> arr[i];
+//    }
+//
+//    for (int i = 0; i < a-1; ++i) {
+//        cout << arr[((i+b) % (a))] << ' ';
+//    }
+//    cout << arr[((a-1+b) % (a))];
+//
+//    return 0;
+//}
+
+
+// another solution ... (slower by almost 73 ms )
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
+    int n,d; cin >> n >> d;
 
-    int a,b;
-    cin >> a >> b;
-    int arr[100005];
-    for (int i = 0; i < a; ++i) {
-        cin >> arr[i];
+    deque<int>dq(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> dq[i];
     }
 
-    for (int i = 0; i < a-1; ++i) {
-        cout << arr[((i+b) % (a))] << ' ';
+    for (int i = 0; i < d; ++i) {
+        dq.push_back(dq.front());
+        dq.pop_front();
     }
-    cout << arr[((a-1+b) % (a))];
 
+    for (int i = 0; i < n-1; ++i) {
+        cout << dq[i] << ' ';
+    }
+    cout << dq[n-1];
     return 0;
 }
