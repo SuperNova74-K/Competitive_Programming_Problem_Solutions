f89e4734f2a56c24ee1a4ac72cdb62a4f9c832c3
SuperNova74-K <kingkhaled.w@gmail.com>
2022-09-17 13:40:53 +0200

    Two Pointers

--- CF_701C_They_Are_Everywhere.cpp
+++ CF_701C_They_Are_Everywhere.cpp
@@ -13,59 +13,32 @@ int main() {
     fio
 
     ll n; cin >> n;
-
-
-    unordered_set<char>set;
     string lol; cin >> lol;
-    lol = "0" + lol;
-
-    for (int i = 1; i <= n; ++i) {
-        set.insert(lol[i]);
+    // Dynamic Sliding Window
+    unordered_set<char>all_uniq_characters;
+    for(char & i : lol){
+        all_uniq_characters.insert(i);
     }
-
-    ll l = set.size() , r = n, answer = INT16_MAX;
-
-    while(l<=r){
-        ll midpoint = (l+r)/2;
-        unordered_map<char,ll>map;
-        ll i = 1,j = 1, current_uniq = 0,max_uniq=0;
-        while(j <= midpoint){
-            if(not map.count(lol[j])){
-                current_uniq++;
-            }
-            map[lol[j]]++;
-            j++;
+    ll n_uniq = all_uniq_characters.size();
+    ll l=0,r=0;
+    ll map[266]{0};
+    ll current_size = 0, answer = n, uniq = 0;
+    while(r < n){
+        if(uniq == n_uniq && current_size == uniq){cout << uniq;return 0;}
+        if(not map[lol[r]]){
+            uniq++;
         }
-
-        max_uniq = max(max_uniq,current_uniq);
-
-        while(current_uniq != set.size() && j<=n){
-            if(not map.count(lol[j])){
-                current_uniq++;
-            }
-            map[lol[j]]++;
-            if(map[lol[i]] < 2){
-                current_uniq--;
-                map.erase(lol[i]);
-            }else{
-                map[lol[i]]--;
-            }
-
-            j++;
-            i++;
-            max_uniq = max(max_uniq,current_uniq);
-        }
-
-
-        if(max_uniq == set.size()){
-            answer = min(answer,midpoint);
-            r = midpoint-1;
-        }else{
-            l = midpoint+1;
+        map[lol[r]]++;
+        r++;
+        current_size++;
+        while(uniq == n_uniq){
+            answer = min(answer, current_size);
+            map[lol[l]]--;
+            if(not map[lol[l]])uniq--;
+            l++;
+            current_size--;
         }
     }
-
     cout << answer;
-
     return 0;
 }
