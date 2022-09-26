4ae53bbf29f4545b14085565d865a57c118a86e1
SuperNova74-K <kingkhaled.w@gmail.com>
2022-09-26 22:17:06 +0200

    brute force

---
+++ CF_1560A_Dislike_of_Threes.cpp
@@ -0,0 +1,71 @@
+// أبدأ بسم الله مستعينا        راض به مُدّبر معينا         
+// و بعد إني باليقين أشهد    شهادة الإخلاص أن لا يُعبد     
+// بالحق مألوه سوى الرحمن    من جل عن عيب و عن نقصان   
+// و أن خير خلقه محمد        من جائنا بالبينات و الهدى 
+
+// Bismillah Al Rahman Al Rahim
+
+// Author           : Khaled Waleed
+// File Created on  : 24/Sep/2022 on 22:05:14
+// University       : Cairo University - Faculty Of Computers and Artificial Intelligence
+// LinkedIn         : https://www.linkedin.com/in/khaled-waleed-salah/
+// Telegram         : https://t.me/SuperNova74_K
+// FEEL free to contact me for any help :P
+
+// This is a Solution for Problem CF_1560A_Dislike_of_Threes
+
+#include<bits/stdc++.h>
+
+#define ll long long
+#define spacebar " "
+#define newline '\n'
+#define fio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ones_in_binary(x) __builtin_popcount(x)
+using namespace std;
+
+
+
+
+int main() {
+    fio
+
+//    int numbers[2000]{0};
+//    numbers[3]++;
+//    for (int i = 0; i < 100; ++i) {
+//        numbers[i*10+3] = 1;
+//    }
+//    for (int i = 1; i <= 333; ++i) {
+//        numbers[i*3]=1;
+//    }
+//
+//    for (int i = 1; i < 2000; ++i) {
+//        numbers[i]+=numbers[i-1];
+//    }
+//
+//    ll t,k; cin >> t;
+//    while(t--){
+//        cin >> k;
+////        ll cntr_of_numbers=0;
+//////        ll answer;
+////        while(cntr_of_numbers!=k){
+////            cntr_of_numbers = numbers[k] + k;
+////        }
+//        ll l = 1,r = 2000;
+//        while(l<=r){
+//            ll midpoint = (l+r)/2;
+//            if(midpoint-numbers[midpoint] > k){
+//                r = midpoint-1;
+//            }else if (midpoint-numbers[midpoint] < k){
+//                l = midpoint+1;
+//            }else{
+//                cout << midpoint << '\n';
+//                break;
+//            }
+//        }
+//    }
+
+
+
+
+    return 0;
+}
